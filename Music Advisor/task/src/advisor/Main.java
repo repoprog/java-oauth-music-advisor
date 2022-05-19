1cbe6137c1b020f50e52b62cf01e3f1195afb051
Remigiusz <repogplus@gmail.com>
2022-05-19 16:51:26 +0200

    Stage completed

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -195,29 +195,3 @@ public class Main {
     }
 }
 
-class Album {
-    private String name;
-    private String artist;
-
-    public Album(String name, String artist) {
-        this.name = name;
-        this.artist = artist;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
-}
-
